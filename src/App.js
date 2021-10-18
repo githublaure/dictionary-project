5733c8ea7b0510482e4d7bb9fe3ec5f36657592f
githublaure <laureagrech@gmail.com>
2021-10-18 21:41:00 +0200

    uploaded dependencies

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import pinkcutefolder from "./pinkcutefolder.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pinkcutefolder} className="App-logo img-fluid" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Search
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
