c39cdc8ed34a9789fa3257f353059a8481692cb5
githublaure <laureagrech@gmail.com>
2021-10-20 12:09:18 +0200

    added searching form and gradient background

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ function App() {
       <div className="container">
         <header className="App-header">
           <img src={pinkcutefolder} className="App-logo img-fluid" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a href="http://wwww.google.fr" className="btn btn-primary">
-            Search
-          </a>
         </header>
         <main>
           <Dictionary />
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
   function search(event) {
     event.preventDefault();
-    alert("searching");
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    console.log(event);
+    setKeyword(event.target.value);
   }
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} />
+        <input type="search" onChange={handleKeywordChange} />
       </form>
     </div>
   );
