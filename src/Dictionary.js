0e6d2cfab0e12f10df34896e1d81c6469c932e79
githublaure <laureagrech@gmail.com>
2021-11-18 07:37:44 +0100

    renamed var

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,38 +5,41 @@ import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
-  let [search, setSearch] = useState("");
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
   let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
-  function handleDisplayPhotos(response) {
+  function handlePexelsResponse(response) {
     setPhotos(response.data.photos);
   }
 
-  function dictionary(event) {
-    event.preventDefault();
+  function search() {
     // API dictionary documentation: https://dictionaryapi.dev/e
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${search}`;
-    axios.get(apiUrl).then(handleResponse);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionResponse);
 
     // API pexels for photos
     let pexelsApiKey =
       "563492ad6f91700001000001a080dcf3127941029259c9153d94df32";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${search}&per_page=9`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handleDisplayPhotos);
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
-    setSearch(event.target.value);
+    event.preventDefault();
+    search();
   }
 
-<<<<<<< HEAD
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   function load() {
     setLoaded(true);
     search();
@@ -66,21 +69,4 @@ export default function Dictionary(props) {
     search();
     return "Loading";
   }
-=======
-  return (
-    <div className="Dictionary">
-      <section>
-        <h1>What word do you want to look up?</h1>
-        <form onSubmit={dictionary} className="form">
-          <input type="search" onChange={handleSubmit} />
-        </form>
-        <div className="hint">
-          suggested words: sunset, wine, yoga, plant...
-        </div>
-      </section>
-      <Results results={results} />
-      <Photos photos={photos} />
-    </div>
-  );
->>>>>>> 009d461d85a89fb3bf4ea1ba7720758268ef7d8b
 }
